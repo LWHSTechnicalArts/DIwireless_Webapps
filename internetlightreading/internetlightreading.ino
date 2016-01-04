433908984c0ef5f7beb511ee25a65f973e1699be
Andrew Kleindolph <akleindolph@gmail.com>
2016-01-03 20:08:59 -0800

    photon code fixed

---
+++ internetlightreading/internetlightreading.ino
@@ -0,0 +1,16 @@
+int reading = 0;
+double volts = 0.0;
+int analogPin = A0;
+
+void setup() {
+    Serial.begin(9600);
+    Spark.variable("analog", &reading, INT);
+    Spark.variable("volts", &volts, DOUBLE);
+}
+
+void loop() {
+  reading = analogRead(analogPin);
+  volts = reading * 3.3 / 4096.0;
+  Serial.println(volts);
+  delay(1000);
+}
